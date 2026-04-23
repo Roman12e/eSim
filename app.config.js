ab7a54fcdd49eaa3ce1a0b3025384bd16445c722
Yuma <romanuminov07165@gmail.com>
2026-04-23 13:53:51 +0300

    Increase app version

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
     name: "TheBestEsim",
     slug: "thebestesim",
-    version: "2.0.0",
+    version: "3.0.0",
     orientation: "portrait",
     icon: "./logo/Logo.png",
     scheme: "thebestesim",
